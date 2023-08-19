647fbbf66909034d28907ba36a41ef0baacdee39
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-08-18 21:45:14 -0500

    create

---
+++ week-2/berryhill_myworld.py
@@ -0,0 +1,10 @@
+#-----------------------------------------------------
+# Title: berryhill_myworld.py
+# Author: Nolan Berryhill
+# Date: 18 August 2023
+# Description: Exercise 2.3 
+#-----------------------------------------------------
+
+last_name = "Berryhill"
+
+print(f"You are now in {last_name}'s world")
