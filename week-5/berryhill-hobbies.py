43d1f2edef4e9ebca273d75ae505dec44a6476aa
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-09-05 16:09:52 -0500

    merge

---
+++ week-5/berryhill-hobbies.py
@@ -0,0 +1,25 @@
+#-----------------------------------------------------
+# Title: berryhill-hobbies.py
+# Author: Nolan Berryhill
+# Date: 10 September 2023
+# Description: Exercise 5.3 
+#-----------------------------------------------------
+
+# List of hobbies
+hobbies = ["Working out", "Video Games", "Watching Movies", "Reading", "Watching Tv Shows"]
+
+# Print hobbies
+print('My Hobbies:')
+for hobby in hobbies:
+    print("- " + hobby)
+
+# List of days
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+# Print day messages
+print("\nDays of the Week:")
+for day in days:
+    if day == "Saturday" or day == "Sunday":
+        print(f"{day}: You are off and should enjoy your hobbies!")
+    else:
+        print(f"{day}: It is a work day.")
