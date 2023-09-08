4eedd9de0a734a29524fb6d5d6198cd0176ae294
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-09-07 19:28:47 -0500

    merge

---
+++ week-5/berryhill-projections.js
@@ -0,0 +1,29 @@
+/**
+	Title: berryhill-projections.js
+    Author: Nolan Berryhill
+    Date: 10 September 2023
+    Description: MongoDB queries for the users collection
+ */
+
+// Add a new user
+const newUser = {
+    "firstName": "Bob",
+    "lastName": "Builder",
+    "employeeId": "1013",
+    "email": "bobbuilder@mail.com",
+    "dateCreated": new Date()
+}
+
+db.users.insertOne(newUser)
+
+// Update Mozart's email address
+db.users.updateOne(
+    { lastName: "Mozart" },
+    { $set: { email: "mozart@me.com" } }
+)
+
+// Verify that Mozart's email address was updated
+db.users.find({ lastName: "Mozart" })
+
+// List all documents in the user's collection with projections
+db.users.find({}, { firstName: 1, lastName: 1, email: 1, _id: 0 })
