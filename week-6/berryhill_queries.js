1d358b3fdad998dae88abd93d314530e5d25632d
Nolan Berryhill <nberryhill@my365.bellevue.edu>
2023-09-16 16:41:41 -0500

    merge

--- week-6/berryhill_queries.js
+++ week-6/berryhill_queries.js
@@ -13,8 +13,8 @@ db.students.find({})
 
 // Display add documents to the houses collection
 const newStudent = {
-    "firstName": "New",
-    "lastName": "Student",
+    "firstName": "Harry",
+    "lastName": "Dresden",
     "studentId": "s1019",
     "houseId": "h1007"
 };
@@ -25,80 +25,10 @@ db.students.insertOne(newStudent);
 db.students.deleteOne({ "studentId": "s1019" });
 
 // Display all students from the houses collection
-db.students.aggregate([
-    {
-        $lookup: {
-            from: "houses",
-            localField: "houseId",
-            foreignField: "houseId",
-            as: "houseInfo"
-        }
-    },
-    {
-        $unwind: "$houseInfo"
-    },
-    {
-        $project: {
-            _id: 0,
-            firstName: 1,
-            lastName: 1,
-            house: "$houseInfo.mascot"
-        }
-    }
-]);
+db.students.aggregate([{ $lookup: { from: "houses", localField: "houseId", foreignField: "houseId", as: "houseInfo" } }, { $unwind: "$houseInfo" }, { $project: { _id: 0, firstName: 1, lastName: 1, house: "$houseInfo.mascot" } }]);
 
 // Display all students from Gryffindor the houses collection
-db.students.aggregate([
-    {
-        $lookup: {
-            from: "houses",
-            localField: "houseId",
-            foreignField: "houseId",
-            as: "houseInfo"
-        }
-    },
-    {
-        $unwind: "$houseInfo"
-    },
-    {
-        $match: {
-            "houseInfo.mascot": "Loin"
-        }
-    },
-    {
-        $project: {
-            _id: 0,
-            firstName: 1,
-            lastName: 1,
-            house: "$houseInfo.mascot"
-        }
-    }
-]);
+db.students.aggregate([{ $lookup: { from: "houses", localField: "houseId", foreignField: "houseId", as: "houseInfo" } }, { $unwind: "$houseInfo" }, { $match: { "houseInfo.mascot": "Lion" } }, { $project: { _id: 0, firstName: 1, lastName: 1, house: "$houseInfo.mascot" } }]);
 
 // Display all students from Eagle mascot house from the houses collection
-db.students.aggregate([
-    {
-        $lookup: {
-            from: "houses",
-            localField: "houseId",
-            foreignField: "houseId",
-            as: "houseInfo"
-        }
-    },
-    {
-        $unwind: "$houseInfo"
-    },
-    {
-        $match: {
-            "houseInfo.mascot": "Eagle"
-        }
-    },
-    {
-        $project: {
-            _id: 0,
-            firstName: 1,
-            lastName: 1,
-            house: "$houseInfo.mascot"
-        }
-    }
-]);
+db.students.aggregate([{ $lookup: { from: "houses", localField: "houseId", foreignField: "houseId", as: "houseInfo" } }, { $unwind: "$houseInfo" }, { $match: { "houseInfo.mascot": "Eagle" } }, { $project: { _id: 0, firstName: 1, lastName: 1, house: "$houseInfo.mascot" } }]);
